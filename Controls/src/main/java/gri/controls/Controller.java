067245ad706543fc775a378af1c2a30bacb2961b
RiviereGregory <gregory.riviere@laposte.net>
2022-10-19 18:07:29 +0200

    Action sur event pour écrit Sysout

--- Controls/src/main/java/gri/controls/Controller.java
+++ Controls/src/main/java/gri/controls/Controller.java
@@ -1,14 +1,7 @@
 package gri.controls;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class Controller {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onButtonClick() {
+        System.out.println("Hello");
     }
 }
