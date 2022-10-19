518407bdc4821288d305c6c3909606f4f48c9ced
RiviereGregory <gregory.riviere@laposte.net>
2022-10-19 18:16:51 +0200

    Action sur event pour écrit Sysout avec récuperation du contenu du textField

--- Controls/src/main/java/gri/controls/Controller.java
+++ Controls/src/main/java/gri/controls/Controller.java
@@ -1,7 +1,14 @@
 package gri.controls;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField namedField;
+
+    @FXML
     public void onButtonClick() {
-        System.out.println("Hello");
+        System.out.println("Hello, " + namedField.getText());
     }
 }
