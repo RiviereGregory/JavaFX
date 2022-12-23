9c47089cb5b3d4ee239e6e735e90109f017682a0
RiviereGregory <gregory.riviere@laposte.net>
2022-12-23 15:18:36 +0100

    ProjetFX Apllication - Ajout selecteur de fichier sur le click du bouton 1

--- JavaFX Application/src/main/java/gri/javafxapplication/Controller.java
+++ JavaFX Application/src/main/java/gri/javafxapplication/Controller.java
@@ -4,13 +4,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Controller {
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     @FXML
     private Label label;
 
     @FXML
     private Button button4;
+
+    @FXML
+    private GridPane gridPane;
 
     public void initialize() {
         button4.setEffect(new DropShadow());
@@ -26,5 +36,22 @@ public class Controller {
     public void handleMouseExit() {
         label.setScaleX(1.0);
         label.setScaleY(1.0);
+    }
+
+    public void handleClick() {
+        //Pour choisir un fichier
+//        FileChooser chooser = new FileChooser();
+//        // gridPane.getScene().getWindow() permet de ne pas ouvrir plusieur explorateur
+//        chooser.showOpenDialog(gridPane.getScene().getWindow());
+
+        //Pour choisir un repertoire
+        DirectoryChooser chooser = new DirectoryChooser();
+        // gridPane.getScene().getWindow() permet de ne pas ouvrir plusieur explorateur
+        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        if (file != null) {
+            LOGGER.log(Level.INFO, "Chemin du repertoire: {0}", file.getPath());
+        } else {
+            LOGGER.log(Level.INFO, "Chooser cancel");
+        }
     }
 }
--- JavaFX Application/src/main/java/module-info.java
+++ JavaFX Application/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module gri.javafxapplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
 
 
     opens gri.javafxapplication to javafx.fxml;
