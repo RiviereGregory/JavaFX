1a20a48de8735f50aa22d0698f060fdedcaff2ce
RiviereGregory <gregory.riviere@laposte.net>
2022-12-23 14:51:17 +0100

    ProjetFX Apllication - utilisation du style caspian

--- JavaFX Application/src/main/java/gri/javafxapplication/Main.java
+++ JavaFX Application/src/main/java/gri/javafxapplication/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
         Scene scene = new Scene(fxmlLoader.load(), 600, 275);
         stage.setTitle("JavaFX Application");
         stage.setScene(scene);
